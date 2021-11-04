9280ecc1a2c890e708ebcda25c763ef5ab455dc7
Anver Bogatov <bogatov.anver@gmail.com>
2021-11-04 09:24:55 +0400

    Initial commit

---
+++ src/main/java/com/anverbogatov/dddyogasessions/DddYogaSessionsApplication.java
@@ -0,0 +1,12 @@
+package com.anverbogatov.dddyogasessions;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DddYogaSessionsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DddYogaSessionsApplication.class, args);
+    }
+}
---
+++ src/test/java/com/anverbogatov/dddyogasessions/DddYogaSessionsApplicationTests.java
@@ -0,0 +1,12 @@
+package com.anverbogatov.dddyogasessions;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DddYogaSessionsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+}
